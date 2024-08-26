c8ca878e09357534afbb1567a444ab4884264b14
Suryaa <0x0is1@protonmail.com>
2024-08-26 13:54:44 +0530

    Created album screen

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import PlayerScreen from './screens/PlayerScreen/PlayerScreen';
 import { StatusBar } from 'expo-status-bar';
 import { PlayerProvider } from './contexts/PlayerContext';
+import AlbumsScreen from './screens/AlbumsScreen/AlbumsScreen';
 
 const Stack = createStackNavigator();
 
@@ -12,8 +13,9 @@ export default function App() {
     <PlayerProvider>
       <StatusBar hidden={true}/>
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="PlayerScreen">
           <Stack.Screen name="PlayerScreen" component={PlayerScreen} />
+          <Stack.Screen name="AlbumsScreen" component={AlbumsScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </PlayerProvider>
---
+++ screens/AlbumsScreen/AlbumsScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AlbumsScreen = () => {
+  return (
+    <View>
+      <Text>AlbumsScreen</Text>
+    </View>
+  )
+}
+
+export default AlbumsScreen
+
+const styles = StyleSheet.create({})
